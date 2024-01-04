d5f416d48a07035020bc9e0357bcc8e2001f57e6
Edilson Marques <marquesedijr@gmail.com>
2024-01-04 14:56:34 -0300

    1

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    # Configurações do aplicativo, como a chave secreta
+    app.config['SECRET_KEY'] = '123'
+
+    # Importa as rotas
+    from app import routes
+
+    return app
